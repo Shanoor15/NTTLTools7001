7f0cbcd1d52fccdeda5703640623b8c00600e281
SHANOOR <SHANOOR@LAPTOP-Q2AOCI85>
2025-05-04 15:00:45 +0530

    Dev1 --Complete the UPIPayment

---
+++ GITProj01/src/main/java/com/nt/upi/UPIPaymentOperation.java
@@ -0,0 +1,9 @@
+package com.nt.upi;
+
+public class UPIPaymentOperation {
+
+	public String doPayment(long phoneNo,double amount)
+	{
+		return amount+ " amount is paid to this phone number  "+phoneNo;
+	}
+}
