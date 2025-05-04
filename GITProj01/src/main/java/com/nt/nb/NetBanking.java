a61843068000f6f5be5685d254d971e427de015d
SHANOOR <SHANOOR@LAPTOP-Q2AOCI85>
2025-05-04 16:32:33 +0530

    TL --reviwed NB code

--- GITProj01/src/main/java/com/nt/nb/NetBanking.java
+++ GITProj01/src/main/java/com/nt/nb/NetBanking.java
@@ -2,8 +2,9 @@ package com.nt.nb;
 
 public class NetBanking {
 
+	//perform NetBanking Process
 	public String transferMoney(long srcAcount,long destAcount,String ifscCode,double amount)
 	{
-		return amount+" amount is transfering from "+srcAcount+" to "+destAcount;
+		return "@"+amount+" amount is transfering from "+srcAcount+" to "+destAcount;
 	}
 }
